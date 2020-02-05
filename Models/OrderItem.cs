3d3372f5ed1f60d0c27875733a4411cc8faf1d7f
davidolawale <olawaledavid11@gmail.com>
2020-02-05 14:36:41 +0100

    Add Product class to replace Device class

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -5,8 +5,8 @@ namespace MobileStoreApp.Models
         public int Id { get; set; }
         public string CustoerId { get; set; }
         public Customer Customer { get; set; }
-        public int DeviceId { get; set; }
-        public Device Device { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
         public int Quantity { get; set; }
         public int OrderId { get; set; }
     }
