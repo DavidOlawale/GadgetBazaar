23abb03c25e8cb3581c67e4fb522e30f80f04df1
davidolawale <olawaledavid11@gmail.com>
2020-02-10 20:27:59 +0100

    Add brand

---
+++ ClientApp/src/app/Services/brand.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Brand } from '../Model/brand';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrandService {
+
+  constructor(private http: HttpClient) {   }
+
+   getBrands(): Observable<Brand[]>{
+     return this.http.get<Brand[]>('/api/brands/getbrands')
+   }
+
+}
