48c7ab15c95ae9b7a834bea3a73a4795e36676c5
davidolawale <olawaledavid11@gmail.com>
2020-04-16 23:49:48 +0100

    Fix minor issues in productService

--- ClientApp/src/app/Services/products-service.service.ts
+++ ClientApp/src/app/Services/products-service.service.ts
@@ -3,44 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from './../Model/product';
 import { Injectable } from '@angular/core';
 import { Brand } from '../Model/brand';
+import { ServerService } from './server.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  constructor(private http: HttpClient){
+  constructor(private server: ServerService) {
   }
-  // products: Product[] = [
-  //   {
-  //     id: 1,
-  //     model: 'Infinix Hot 7',
-  //     brandId: 3,
-  //     brand: new Brand(),
-  //     price: 45000
-  //   },
-  //   {
-  //     id: 2,
-  //     model: 'Iphone 11 pro',
-  //     brandId: 1,
-  //     brand: new Brand(),
-  //     price: 300000
-  //   },
-  //   {
-  //     id: 3,
-  //     model: 'Samsung S 10',
-  //     brandId: 2,
-  //     brand: new Brand(),
-  //     price: 80000
-  //   },
-  //   {
-  //     id: 4,
-  //     model: 'Iphone x max',
-  //     brandId: 2,
-  //     brand: new Brand(),
-  //     price: 300000
-  //   }
-  // ]
+
   getProducts(): Observable<Product[]>{
-    return this.http.get<Product[]>('/api/products')
+    return this.server.get('/products')
   }
 }
