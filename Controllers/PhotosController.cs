2257b105017418520b78ab56d7be53a985adb03d
davidolawale <olawaledavid11@gmail.com>
2020-04-16 19:00:49 +0100

    Enhance PhotoComponent

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -7,35 +7,45 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MobileStoreApp.Data;
+using MobileStoreApp.Models;
 
 namespace MobileStoreApp.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
     public class PhotosController : ControllerBase
     {
         private readonly IWebHostEnvironment hostEnvironment;
+        private readonly ApplicationDbContext dbContext;
 
-        public PhotosController(IWebHostEnvironment hostEnvironment)
+        public PhotosController(IWebHostEnvironment hostEnvironment, ApplicationDbContext dbContext)
         {
             this.hostEnvironment = hostEnvironment;
+            this.dbContext = dbContext;
         }
 
         [Authorize(Roles = "Admin")]
-        [Route("products/{productId}/photos")]
+        [HttpPost("products/{productId}/photos")]
         public ActionResult PostProductImage(int productId, IFormFile photo)
         {
             // wwwwroot/images
-            var productImagesDirectory = Path.Combine(hostEnvironment.WebRootPath, "Images", "Products");
-            var newImageName = new Guid().ToString() + Path.GetExtension(photo.FileName);
+            var imagesDirectory = Path.Combine(hostEnvironment.WebRootPath, "Images", "Products");
+            if (!Directory.Exists(imagesDirectory))
+                Directory.CreateDirectory(imagesDirectory);
 
-            var newImagePath = Path.Combine(productImagesDirectory, newImageName);
+            var newImageName = Guid.NewGuid().ToString() + Path.GetExtension(photo.FileName);
+
+            var newImagePath = Path.Combine(imagesDirectory, newImageName);
             using (var stream = new FileStream(newImagePath, FileMode.Create))
             {
                 photo.CopyTo(stream);
             }
+            var image = new ProductImage { Name = newImageName, ProductId = productId };
+            dbContext.ProductImages.Add(image);
+            dbContext.SaveChanges();
 
-            return Created("", new object { });
+            return Created("", image);
         }
     }
 }
