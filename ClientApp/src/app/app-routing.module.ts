cea5adc43ad20f02767b7ec7050a681cada5d98f
davidolawale <olawaledavid11@gmail.com>
2020-04-21 21:22:45 +0100

    Add new-product component

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { EditProductComponent } from './admin/edit-product/edit-product.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './customer/cart/cart.component';
 import { LoginComponent } from './login/login.component';
+import { NewProductComponent } from './admin/new-product/new-product.component';
 
 
-const routes = [
+const routes: Route[] = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'products/new', component: NewProductComponent },
+  { path: 'products/:id', component: ProductDetailsComponent, pathMatch: 'full' },
   { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'products/:id/edit', component: EditProductComponent },
-  { path: 'product/:id/edit', component: EditProductComponent }
+  { path: 'products/:id/edit', component: EditProductComponent }
 ]
 
 @NgModule({
