832a509cab1736259e5ca80daa6021f10459cec5
davidolawale <olawaledavid11@gmail.com>
2020-04-23 23:41:12 +0100

    Add photo-service

---
+++ ClientApp/src/app/Services/photo-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ServerService } from './server.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoServiceService {
+
+  constructor(private server: ServerService) {
+
+  }
+
+  getProductImages(id: Number) {
+    return this.server.get(`/products/${id}/photos`).toPromise()
+  }
+
+  postProductImage(id: Number, photo) {
+    let formData = new FormData()
+    formData.append('photo', photo)
+    return this.server.post(`/products/${id}/photos`, formData).toPromise()
+  }
+}
