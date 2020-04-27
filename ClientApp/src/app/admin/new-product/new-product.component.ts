8df466eefddddef9bb119d5fef7bc97e5d93b4af
davidolawale <olawaledavid11@gmail.com>
2020-04-27 16:16:59 +0100

    Add toast notification to admin/new-product component

--- ClientApp/src/app/admin/new-product/new-product.component.ts
+++ ClientApp/src/app/admin/new-product/new-product.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ProductsService } from '../../Services/products-service.service';
 import { Brand } from '../../Model/brand';
 import { ServerService } from '../../Services/server.service';
+import { ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-new-product',
@@ -13,7 +14,11 @@ import { ServerService } from '../../Services/server.service';
 export class NewProductComponent implements OnInit {
   private product: Product = new Product()
   private brands: Brand[]
-  constructor(private router: Router, private server: ServerService, private productService: ProductsService) { }
+
+  constructor(private router: Router,
+    private server: ServerService,
+    private productService: ProductsService,
+    private toastyService: ToastyService) { }
 
   async ngOnInit() {
     this.brands = await this.productService.getProductBrands()
@@ -24,11 +29,24 @@ export class NewProductComponent implements OnInit {
   }
 
   onSubmitSucceded = () => {
+    this.toastyService.success({
+      title: 'Success',
+      msg: 'Product added successfully',
+      showClose: true,
+      theme: 'bootstrap',
+      timeout: 3000
+    })
     this.router.navigate(['/products'])
   }
 
   onSubmitFailed =() => {
-    alert('failed')
+    this.toastyService.success({
+      title: 'Error',
+      msg: 'An error occured',
+      showClose: true,
+      theme: 'bootstrap',
+      timeout: 3000
+    })
   }
 
 
