937136a0ac28809f62800b3edd5cf5ca85b87deb
davidolawale <olawaledavid11@gmail.com>
2020-04-28 13:44:19 +0100

    Fix issue where styles in app-component styles are not being applied in sub components

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app';
